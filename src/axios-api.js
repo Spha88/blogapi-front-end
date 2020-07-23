c6850b55bb5e965890db2f308fea22a8dd224f3f
Siphamandla <mr.smehlomakulu@gmail.com>
2020-07-23 22:28:48 +0200

    Change base uri on axios instance

--- src/axios-api.js
+++ src/axios-api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const token = localStorage.getItem('myJwt');
 
-const url = process.env.REACT_APP_API_BASE_URL || process.env.API_BASE;
+// const url = process.env.REACT_APP_API_BASE_URL || process.env.API_BASE;
 
 const instance = axios.create({
-    baseURL: url,
+    baseURL: 'https://myblogapi.herokuapp.com',
     headers: {
         'Authorization': `Bearer ${token}`
     }

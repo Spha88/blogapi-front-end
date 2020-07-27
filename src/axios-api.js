b1f0a457cd1e3e7023c33b49b245c8d5fc8fbcd4
Siphamandla <mr.smehlomakulu@gmail.com>
2020-07-27 05:18:06 +0200

    Correctly use the env file for environment variables

--- src/axios-api.js
+++ src/axios-api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const token = localStorage.getItem('myJwt');
 
-// const url = process.env.REACT_APP_API_BASE_URL || process.env.API_BASE;
+const url = process.env.REACT_APP_API_BASE_URL || process.env.REACT_APP_API_BASE_LOCAL;
 
 const instance = axios.create({
-    baseURL: 'https://myblogapi.herokuapp.com',
+    baseURL: url,
     headers: {
         'Authorization': `Bearer ${token}`
     }
